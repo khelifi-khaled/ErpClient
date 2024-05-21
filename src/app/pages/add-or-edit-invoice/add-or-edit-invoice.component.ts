99329371d11e41e8a1cb4baab596314e3aaf562c
Khaled KHELIFI <khelifi.khaled55@outlook.com>
2024-05-21 13:35:37 +0200

    add or edit invoice WIP

--- src/app/pages/add-or-edit-invoice/add-or-edit-invoice.component.ts
+++ src/app/pages/add-or-edit-invoice/add-or-edit-invoice.component.ts
@@ -5,8 +5,10 @@ import { NbToastrService } from '@nebular/theme';
 import { Customer } from 'src/app/models/customer.models';
 import { Invoice } from 'src/app/models/invoice.models';
 import { InvoiceItem } from 'src/app/models/invoiceItem.models';
+import { Item } from 'src/app/models/item.models';
 import { CustomersService } from 'src/app/services/customer.service';
 import { InvoiceService } from 'src/app/services/invoice.service';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-add-or-edit-invoice',
@@ -21,12 +23,15 @@ export class AddOrEditInvoiceComponent implements OnInit , OnDestroy{
   searchText = '';
   selectedInvoice! : Invoice;
   tax : number = 0;
+  allItems: Item[] = [];
+  filteredItems: Item[] = [];
 
 constructor(
   private readonly _invoiceService : InvoiceService,
   private readonly _toaster: NbToastrService,
   private readonly _router : Router,
-  private readonly _customerService : CustomersService
+  private readonly _customerService : CustomersService,
+  private readonly _itemService : ItemsService
 ) {
 }
   ngOnDestroy(): void {
@@ -40,6 +45,15 @@ constructor(
     this._customerService.getAllCustemers().subscribe({
       next: (customers: Customer[]) => {
         this.allCustomers = customers;
+      },
+      error: (error) => {
+        this._toaster.danger(error.message);
+      }
+    });
+
+    this._itemService.getAllItems().subscribe({
+      next: (items: Item[]) => {
+        this.allItems = items;
       },
       error: (error) => {
         this._toaster.danger(error.message);
@@ -73,10 +87,10 @@ constructor(
       this.calculTotalAmount();
       return;
     }
-    this._toaster.warning('You can not remove all items');
+    this._toaster.info('You can delete item');
   }
 
-onKeyDown(event: any) {  
+onKeyDownCustomer(event: any) {  
   this.filteredCustomers = 
   this.allCustomers
   .filter(
@@ -86,6 +100,43 @@ onKeyDown(event: any) {
       c.customerNumber.toString().includes(event.textContent.toLowerCase())
     ); 
 }
+
+onKeyDownItem(event: any) {  
+  this.filteredItems = 
+  this.allItems
+  .filter(
+    i => 
+      i.itemDiscription.toLowerCase().includes(event.textContent.toLowerCase()) ||
+      i.itemNumber.toLowerCase().includes(event.textContent.toLowerCase()) ||
+      i.supplierItemNumber.toLowerCase().includes(event.textContent.toLowerCase())
+    ); 
+    console.log(this.filteredItems);
+    
+}
+
+addItemToInvoice(item: Item) : void {
+  const invoiceItem : InvoiceItem = {
+    id: 0,
+    item : item,
+    quantity : 1,
+    price : item.itemPrice,
+    purchcasePrice : item.itemPrice + (item.itemPrice * Number.parseInt(item.vatType.vatValue.vatValue)/100),
+    discount : 0
+  };
+
+  const itemToAdd = this.selectedInvoice.invoiceItems.find(i => i.item.id === item.id);
+  if(itemToAdd){
+    this.addQuantity(itemToAdd);
+    this._invoiceService.setInvoice(this.selectedInvoice);
+    this.filteredItems = []
+    return;
+  }
+  this.selectedInvoice.invoiceItems.push(invoiceItem);
+  this.calculTotalAmount();
+  this._invoiceService.setInvoice(this.selectedInvoice);
+  this.filteredItems = []
+}
+
 
 
 setCustomerToInvoice(customer: Customer) : void {
@@ -105,17 +156,10 @@ removeInvoiceItem (id : any) : void {
   this._toaster.warning('You can not remove all items');
 }
 
-
-AddInvoiceItem () : void {
-console.log('AddInvoiceItem');
-
-}
-
 calculTotalAmount () : void {
   this.selectedInvoice.totalAmountExcVat = this.selectedInvoice.invoiceItems.reduce((acc, item) => acc + item.price, 0);
   this.selectedInvoice.totalVatAmount = this.selectedInvoice.invoiceItems.reduce((acc, item) => acc + item.purchcasePrice, 0);
   this.tax = this.selectedInvoice.totalVatAmount - this.selectedInvoice.totalAmountExcVat;
   this._invoiceService.setInvoice(this.selectedInvoice);
 }
-
 }
